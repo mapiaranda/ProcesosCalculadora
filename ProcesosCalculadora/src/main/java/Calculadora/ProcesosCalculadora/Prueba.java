45204201577fba3532c4010ea8686ed145592dad
Mapi <Mapi@172.19.179.34>
2018-10-03 20:15:54 +0200

    prueba3

--- ProcesosCalculadora/src/main/java/Calculadora/ProcesosCalculadora/Prueba.java
+++ ProcesosCalculadora/src/main/java/Calculadora/ProcesosCalculadora/Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Hola mund sdfjsladkjfoñalsdjfñaisjdo");
+		System.out.print("Hola mundo");
 
 	}
 
